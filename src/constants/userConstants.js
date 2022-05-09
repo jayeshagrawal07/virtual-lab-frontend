ec31a3f238d9d16d0418dd1cd2eab07ab21996e1
Jayesh Agrawal <jayeshagrawal07@gmail.com>
2022-05-09 16:04:52 +0530

    data changes

--- src/constants/userConstants.js
+++ src/constants/userConstants.js
@@ -8,3 +8,9 @@ export const USER_LOGIN_SUCCESS = 'USER_LOGIN_SUCCESS'
 export const USER_LOGIN_FAIL = 'USER_LOGIN_FAIL'
 
 export const USER_LOGOUT = 'USER_LOGOUT'
+
+export const TYPES = {
+    DFA: "DFA",
+    NDFA: "NDFA",
+    DFA_NFA: "DFA_NFA",
+}
--- src/screen/ExperimentScreen.js
+++ src/screen/ExperimentScreen.js
@@ -1,30 +1,46 @@
-import React from 'react'
-import experiments from '../experiments'
-import { Link }  from "react-router-dom"
-import {Container  ,Button} from "react-bootstrap"
-function ExperimentScreen({experiment}) {
+import React from "react";
+import experiments from "../experiments";
+import { Link } from "react-router-dom";
+import { Container, Button } from "react-bootstrap";
+function ExperimentScreen({ experiment }) {
   return (
     <Container>
-  
-    <div className="row mb-2">
-       {experiments.map (experiment =>(
-    <div className="col-md-6" key ={experiment._id}>
-      <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-lg h-md-250 position-relative">
-        <div className="col p-4 d-flex flex-column position-static">  
-          <h3 className="mb-0">{experiment.Objective}</h3>
-          <p className="card-text mb-auto"> {experiment.Intro}</p>
-         <hr/>
-         <div className="d-flex gap-1  justify-content-end">
-            <Link className="btn btn-primary" to={`/experiment/${experiment._id}`}>Continue Reading</Link>
-          <Button>Simulation</Button>
+      <div className="row mb-2">
+        {experiments.map((experiment) => (
+          <div className="col-md-6" key={experiment._id}>
+            <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-lg h-md-250 position-relative">
+              <div className="col p-4 d-flex flex-column position-static">
+                {/*<h3 className="mb-0">{experiment.Objective}</h3>*/}
+                <h3 className="mb-0" title={experiment.Objective}>
+                  {experiment.Objective.length > 50
+                    ? `${experiment.Objective.slice(0, 50)}...`
+                    : experiment.Objective}
+                </h3>
+                <p className="card-text mb-auto">
+                  {" "}
+                  {experiment.Intro.length > 300
+                    ? `${experiment.Intro.slice(0, 300)}...`
+                    : experiment.Intro}
+                </p>
+                <hr />
+                <div className="d-flex gap-1  justify-content-end">
+                  <Link
+                    className="btn btn-primary"
+                    to={`/experiment/${experiment._id}`}
+                  >
+                    Continue Reading
+                  </Link>
+                  {experiment.simulation && <a href="">Simulation</a>}
+                  {/*experiment.simulation && <a href="">Simulation</a>*/}
+                  {/*experiment.simulation && <Link to={"simulation"}>Simulation</Link>*/}
+                </div>
+              </div>
+            </div>
           </div>
-          </div>
+        ))}
       </div>
-    </div>
-       ))}
-  </div>
-  </Container>
-  )
+    </Container>
+  );
 }
 
-export default ExperimentScreen
+export default ExperimentScreen;
