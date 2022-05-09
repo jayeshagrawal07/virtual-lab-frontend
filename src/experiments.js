b10fa145440cf5951b87b4fc0aed80eeddd907d0
Vishwas Katiyar <honeykatiyar1436@gmail.com>
2022-05-09 23:59:24 +0530

    jpg updates

--- src/experiments.js
+++ src/experiments.js
@@ -27,15 +27,15 @@ const experiments = [
 		Requirement: ' Prior knowledge of Deterministic Finite Automata (DFA)',
 		paragraph1:
 			'The simplest type to begin with is Finite Automata, which is the first option from the selection menu. In JFLAP both DFA and NFA are created using Finite Automata. <br> Now you should have an empty window in front of you. You will have a couple of tools and features at your disposal. The toolbar contains six tools, which are used to edit automata.Attribute Editor Tool, changes properties and position of existing states and transitions. State Creator Tool, creates new states. Transition Creator Tool, creates transitions. Deletion Tool, deletes states and transitions. Undo/Redo, changes the selected object prior to their history.Creating an automaton is easy with the state and transition tools. Note that you need to change back to the Attribute Editor Tool (first) to change states. Let’s try to add states with the State Creator Tool (second).When adding states they automatically get a name assigned to them, which can be changed using the Attribute Editor Tool.',
-		img1: '/images/exp2/exp2.1.JPG',
+		img1: '/images/exp2/exp2.1.jpg',
 
-		img2: '/images/exp2/exp2.2.JPG',
+		img2: '/images/exp2/exp2.2.jpg',
 		paragraph2:
 			'Transitions are easily dragged between states with the Transition Creator Tool.The automaton above only accepts strings containing b which end with a. To test this automaton you  can use any of the available tools under the Input menu.The fastest in this case is to use Input → Fast Run. A  menu where you can set your input string pops  up. Type the string “bba” and select “OK”. The program shows all the transitions that are done when consuming the input string.If you want to test multiple inputs at once you can select the “Multiple Run” option.',
 		paragraph3:
 			'If you wish to individually review single runs, it can be accessed by selecting the View “Trace”, which gives a view similar to the “Fast Run” option.To debug the automata “Step by Closure” or “Step by State” can be selected. For each step the automaton highlights the currently active state.For “stepping” you press the “Step” button.The first character consumed. Notice that the first letter is grayed out and the currently active state    have changed.Now the last character is about to be consumed. This step shows the transition between q0 and q1. If there happen to be multiple paths with the same character you will see them grayed out.The simulator reached the final state. ',
 
-		img3: '/images/exp2/exp2.3.JPG',
+		img3: '/images/exp2/exp2.3.jpg',
 
 		simulation: TYPES.DFA
 	},
@@ -82,8 +82,8 @@ const experiments = [
 		Requirement: 'Study the functionality of Moore machine.',
 		paragraph1:
 			'In the theory of computation, a Moore machine is a finite state machine where the outputs are determined by the current state alone (and do not depend directly on the input). The state diagram for a Moore machine will include an output signal for each state, Compared with a Mealy machine, which maps transitions in the machine to outputs.The advantage of the Moore model is a simplification of the behavior. <br> After completion of this experiment, students are able to design moore machine using Verilog code.',
-		img1: '/images/exp8/exp8-1.JPG',
-		img2: '/images/exp8/exp8-2.JPG',
+		img1: '/images/exp8/exp8-1.jpg',
+		img2: '/images/exp8/exp8-2.jpg',
 		rating: 4.5,
 		numReview: 13,
 		descriptions: 'Bluetooth technology lets you conntct it with compatible device ',
@@ -98,7 +98,7 @@ const experiments = [
 		Requirement: 'Study the functionality of Mealy machine.',
 		paragraph1:
 			'The state diagram for a Mealy machine associates an output value with each transition edge, in contrast to the state diagram for a Moore machine, which associates an output value with each state. <br> When the input and output alphabet are both Σ, one can also associate to a Mealy Automata an Helix directed graph[clarification needed] (S × Σ, (x, i) → (T(x, i), G(x, i))).[2] This graph has as vertices the couples of state and letters, every nodes are of out-degree one, and the successor of (x, i) is the next state of the automata and the letter that the automata output when it is instate x and it reads letter i. This graph is a union of disjoint cycles if the automaton is bireversible.',
-		img1: '/images/exp9/exp9-1.JPG',
+		img1: '/images/exp9/exp9-1.jpg',
 		heading2: 'Mealy Machine –',
 		paragraph2:
 			'1. Output depends on present state as well as present input.<br>2. If input changes, output also changes. <br>3. Less number of states are required.<br> 4. There is less hardware requirement. <br>5. They react faster to inputs.<br> 6. Asynchronous output generation. <br>7. Output is placed on transitions. <br>8. It is difficult to design.',
@@ -130,11 +130,11 @@ const experiments = [
 		Requirement: 'Prior knowledge of Regular Expressions and FSM',
 		paragraph1:
 			'Converting FA to Regular Expression <br> Follow the instructions above the toolbar. To make the conversion work, empty transitions must be  added between states that have yet no transition. States that either is Initial or Final must be removed which you do with the collapse state tool. With the collapse tool you can use the table to inspect combined transitions from that state. The state is removed with The Finalize button.',
-		img1: '/images/exp6/exp6-1.JPG',
-		img2: '/images/exp6/exp6-2.JPG',
+		img1: '/images/exp6/exp6-1.jpg',
+		img2: '/images/exp6/exp6-2.jpg',
 		paragraph2:
 			'When all the necessary steps are made, the  converted automaton  contains  the  regular expression. You can also see the complete regular expression above the toolbar that  can  be  exported  using  Export.',
-		img3: '/images/exp6/exp6-3.JPG',
+		img3: '/images/exp6/exp6-3.jpg',
 		rating: 4.5,
 		numReview: 13,
 		descriptions: 'Bluetooth technology lets you conntct it with compatible device ',
@@ -148,8 +148,8 @@ const experiments = [
 		Requirement: 'Prior knowledge of Regular Grammar and FSM.',
 		paragraph1:
 			'Converting FA to a Grammar <br> When using a Finite Automaton select Convert → Convert to Grammar. The conversion  view  will contain your automata on the left and the grammar on the right. You are free to edit the grammar  yourself or let JFLAP more or less do the work. <br> The “What’s Left?” option will show which transition that not have been used in the grammar yet. JFLAP automatically puts labels to states to tell which symbols they represent in the grammar.As mentioned you can either edit the right side grammar table or click on states to automatically reveal the grammar for each step. The  Hint  reveals which state you should select next. Show All automatically creates the grammar for you.<br> Once the grammar is complete you can select Export to open a new JFLAP window with your new Grammar, don’t forgot to save if you want your grammar saved.',
-		img1: '/images/exp7/exp7-1.JPG',
-		img2: '/images/exp7/exp7-2.JPG',
+		img1: '/images/exp7/exp7-1.jpg',
+		img2: '/images/exp7/exp7-2.jpg',
 		rating: 4.5,
 		numReview: 13,
 		descriptions: 'Bluetooth technology lets you conntct it with compatible device ',
